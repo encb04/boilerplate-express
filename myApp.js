4d96a2765500bf6247eccac60957c06b769afe1e
encb04 <elizabethcarrsco@Elizabeth>
2026-03-23 17:43:42 -0500

    challenge completed

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 console.log("Hello World");
 let express = require('express');
 let app = express();
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello JSON"});
+});
 app.get("/",function(req,res){
     const absolutePath = __dirname+ "/views/index.html";
     res.sendFile(absolutePath);
